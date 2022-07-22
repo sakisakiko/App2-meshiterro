0fe7ddbb9326d12307cae3d11f42b4e8d1cfb972
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-07-22 03:39:42 +0000

    [Finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'homes/about'=>'homes#new',as: 'about'
   resources :post_images,only:[:new,:create,:index,:show,:destroy] do
    resources :post_comments,only:[:create,:destroy]
+   resource :favorites,only: [:create,:destroy]
   end
   
   resources :users,only:[:show,:edit,:update]
